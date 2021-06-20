fdf83f24c129c9614a74b577285fa22991f9b5b9
fransHalason <franshalasonn@gmail.com>
2021-06-20 17:46:53 +0700

    style guide python

---
+++ Machine Learning/Memulai Pemrograman Dengan Python/4. Style Guide pada Python/kal.py
@@ -0,0 +1,27 @@
+class kalkulator:
+    """kalkulator tambah kurang"""
+    def __init__(self, _i = O):
+        self.i = _i
+    def tambah(self, _i): return self.i + _i
+    def kurang(self, _i):
+        return self.i - _i
+    
+# pengecekan style guide pep8
+# pycodestyle
+# pycodestyle kal.py
+
+# pylint
+# pylint kal.py
+
+# flake8
+# flake8 kal.py
+
+# memformat kode
+# black
+# black kal.py
+
+# yapf
+# yapf kal.py
+
+# autopep8
+# autopep8 kal.py
